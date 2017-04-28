1d0ee98771823449115be914afd5337a89e4641f
jeanettepranin <jeanettepranin@gmail.com>
2017-04-28 00:03:52 -0500

    create separate mod files for request and response handling. also wrote down plan of attack

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,45 @@ use std::sync::{Arc,Mutex};
 use std::net::{TcpListener,TcpStream};
 use std::io::{Read,Write,BufReader,BufRead,BufWriter};
 
+mod reqhandler;
+mod reshandler;
+
 //with the help of https://dfockler.github.io/2016/05/20/web-server.html
 
 //In return to a valid GET request, the web server spawns a thread that retrieves the request, records it to a log file, and generates a response. 
+
+
+
 fn main() {
 
 	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+	println!("Listening on port 8080...");
+
+	let log_file_raw = File::create("logs/log.txt").unwrap();
+	let log_file = Arc::new(Mutex::new(log_file_raw));
+	println!("Log file created in /logs/log.txt");
+
 	for stream in listener.incoming() {
+		let log_file = log_file.clone();
+		println!("New connection, thread spawned");
+
 		match stream {
-	        Ok(stream) => {
-	            handle_request(stream);
-	        }
-	        Err(e) => { /* connection failed */ }
-	    }
+			Ok(mut stream) => {
+				handle_client(&mut stream);
+			}
+			Err(e) => {
+				println!("Connection failed! Try again later...");
+			}
+		}
+
 	}
 }
 
-fn handle_request(mut stream: TcpStream) {
-	thread::spawn(move || {
-		let mut bufreader = BufReader::new(stream);
-		for line in bufreader.by_ref().lines() {
-			if line.unwrap() == "" {break;}
-        }
-        send_res(bufreader.into_inner());
-    });
+fn handle_client(stream: &mut TcpStream) {
+	//get req (by line) from stream
+	//check for 400 bad request error
+	//then check if file exists (404 Not Found error)
+	//then check whether it is off limits (403 Forbidden)
+	//if no error, lock and modify log file then print response to stream 
+	//otherwise, print error response
 }
-
-fn send_res(mut stream: TcpStream) {
-	let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, World!</body></html>";
-    stream.write_all(response.as_bytes()).unwrap();
-}
-
-// fn log_request(data: &str) {
-// 	let file = File::create("log.txt").expect("Unable to create log file");
-//     let mut fwriter = BufWriter::new(file);
-//     fwriter.write_all(data.as_bytes()).expect("Unable to write data");
-// }
