2f9ae8dd771c9684bf189cfb19a2518e3839df9b
jeanettepranin <jeanettepranin@gmail.com>
2017-04-27 23:26:51 -0500

    server now works with reloading

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write, BufReader, BufRead};
+use std::thread;
+use std::fs::File;
+use std::sync::{Arc,Mutex};
+use std::net::{TcpListener,TcpStream};
+use std::io::{Read,Write,BufReader,BufRead,BufWriter};
 
+//with the help of https://dfockler.github.io/2016/05/20/web-server.html
+
+//In return to a valid GET request, the web server spawns a thread that retrieves the request, records it to a log file, and generates a response. 
 fn main() {
 
-	let tcp_listen = TcpListener::bind("127.0.0.1:8080").unwrap();
-	let stream = tcp_listen.accept().unwrap().0;
-
-	manage_req(stream);
+	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+	for stream in listener.incoming() {
+		match stream {
+	        Ok(stream) => {
+	            handle_request(stream);
+	        }
+	        Err(e) => { /* connection failed */ }
+	    }
+	}
 }
 
-fn manage_req(stream: TcpStream) {
-
-	let mut bufreader = BufReader::new(stream);
-
-	for line in bufreader.by_ref().lines() {
-        if line.unwrap() == "" {
-            break;
+fn handle_request(mut stream: TcpStream) {
+	thread::spawn(move || {
+		let mut bufreader = BufReader::new(stream);
+		for line in bufreader.by_ref().lines() {
+			if line.unwrap() == "" {break;}
         }
-    }
-
-	send_res(bufreader.into_inner());
+        send_res(bufreader.into_inner());
+    });
 }
 
 fn send_res(mut stream: TcpStream) {
 	let response = "HTTP/1.1 200 OK\n\n<html><body>Hello, World!</body></html>";
     stream.write_all(response.as_bytes()).unwrap();
 }
+
+// fn log_request(data: &str) {
+// 	let file = File::create("log.txt").expect("Unable to create log file");
+//     let mut fwriter = BufWriter::new(file);
+//     fwriter.write_all(data.as_bytes()).expect("Unable to write data");
+// }
